47966cb4c5c2d61ff94bac264ae7482b41a70e63
Mario Adamo Jr <marioadamojr@gmail.com>
2019-07-13 16:40:37 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -17,6 +17,9 @@ class OwnersController < ApplicationController
 
   post '/owners' do
     @owner = Owner.create(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets << @pet.create(params[:pet][:name])
+    end
     redirect "owners/#{owner.id}"
   end
 
