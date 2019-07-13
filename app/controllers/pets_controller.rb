13776a216351b6f8baf5b167322063c7c8ccc8de
Mario Adamo Jr <marioadamojr@gmail.com>
2019-07-13 18:21:00 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -37,9 +37,8 @@ class PetsController < ApplicationController
     end
 
     @pet = Pet.find(params[:id])
-    @pet.update(params[:pet])
-    # @pet.update(name: params[:pet][:name])
-    # @pet.update(owner_id: params[:pet][:owner_id])
+    @pet.update(name: params[:pet][:name])
+    @pet.update(owner_id: params[:pet][:owner_id])
 # binding.pry
 
     if !params[:pet][:owner_id].empty?
